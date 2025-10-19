95cf5334fcac5237c0d160e4bae2e4e042142323
YonesF <yonesmf@uia.no>
2025-10-19 20:43:27 +0200

    (Rapport) - Opprettet ReportController (#2)

---
+++ Kartverket.Web/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kartverket.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Archive()
+        {
+            return View();
+        }
+    }
+}
