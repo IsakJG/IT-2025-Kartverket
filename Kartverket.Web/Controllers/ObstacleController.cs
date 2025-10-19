571e0d952583157b3dca966e16b257a5f6103794
YonesF <yonesmf@uia.no>
2025-10-20 01:26:58 +0200

    La till kommentarer/Dokumentasjon for hver linje i "ObstacleController" for å gjøre det lettere å ha oversikt.

--- Kartverket.Web/Controllers/ObstacleController.cs
+++ Kartverket.Web/Controllers/ObstacleController.cs
@@ -8,22 +8,22 @@ public class ObstacleController : Controller
 {
     // blir kalt etter at vi trykker på "Register Obstacle" lenken i Index viewet
     [HttpGet]
-    public ActionResult DataForm()
+    public ActionResult DataForm() // GET
     {
-        return View();
+        return View(); // Returnerer DataForm viewet
     }
 
 
     // blir kalt etter at vi trykker på "Submit Data" knapp i DataForm viewet
     [HttpPost]
-    public ActionResult DataForm(ObstacleData obstacledata)
+    public ActionResult DataForm(ObstacleData obstacledata) // POST
     {
-        bool isDraft = false;
-        if (obstacledata.ObstacleDescription == null)
+        bool isDraft = false; // Variabel for å sjekke om data er utkast
+        if (obstacledata.ObstacleDescription == null) // Sjekker om beskrivelsen er tom
         { 
-            isDraft = true;
+            isDraft = true; // Setter isDraft til true hvis beskrivelsen er tom
         }
 
-        return View("Overview", obstacledata);
+        return View("Overview", obstacledata); // Returnerer Overview viewet med obstacledata
     }
 }
