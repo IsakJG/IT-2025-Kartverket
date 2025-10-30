f24b608eeac1d9c795f873cf0ab2d4691fec88c3
Tam Nguyen <tamnguyen@Tam-sin-Air.home>
2025-10-30 22:57:45 +0100

    Oppdatert av databaetilkobling

--- Kartverket.Web/Controllers/HomeController.cs
+++ Kartverket.Web/Controllers/HomeController.cs
@@ -5,48 +5,55 @@ using MySqlConnector;
 
 namespace Kartverket.Web.Controllers;
 
-public class HomeController : Controller 
+public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger; // Logger for logging information
     private readonly IConfiguration config; // Configuration for accessing app settings
-
+   
     //private readonly string _connectionString;
-
-    public HomeController(ILogger<HomeController> logger, IConfiguration config) // Konstruktør for HomeController
-    {
-        _logger = logger; // Initialiserer logger
-        this.config = config; // Initialiserer konfigurasjon
-    }
-
-    /*
-    // Send avhengighet til konstruktøren
-        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+    
+        public HomeController(ILogger<HomeController> logger, IConfiguration config) // Konstruktør for HomeController
         {
-            _logger = logger;
-            _connectionString = config.GetConnectionString("DefaultConnection")!;
+            _logger = logger; // Initialiserer logger
+            this.config = config; // Initialiserer konfigurasjon
         }
+        
+
+        /*
+    // Send avhengighet til konstruktøren
+    public HomeController(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DefaultConnection")!;
+
+    }
     */
+
+    
     public IActionResult Index() // Hovedsiden
     {
         return View(); // Returnerer Index viewet
     }
-/*
-//databasen koblingstest 
-public async Task<IActionResult> Index()
+
+    //databasen koblingstest 
+    /*
+    public async Task<IActionResult> Index()
     {
         try
         {
             await using var conn = new MySqlConnection(_connectionString);
             await conn.OpenAsync();
             return Content("✅ Connection to MariaDB successful!");
+
         }
         catch (Exception ex)
         {
             return Content($"❌ Connection failed: {ex.Message}");
+
         }
-        
+
+
     }
-    */
+*/
     public IActionResult GetAThing(int id) // Eksempelmethode som tar en id som parameter
     {
         _logger.LogInformation("GetAThing called with id {Id}", id); // Logger informasjon om kall
@@ -60,7 +67,7 @@ public async Task<IActionResult> Index()
 
     public IActionResult MainPage() // En annen side
     {
-    return View(); // Returnerer MainPage viewet
+        return View(); // Returnerer MainPage viewet
     }
 
 
@@ -75,3 +82,4 @@ public async Task<IActionResult> Index()
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }); // Returnerer Error viewet med RequestId
     }
 }
+
