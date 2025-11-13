5a664199979edf6b1b4204f132c56efdcbc50797
YonesF <yonesmf@uia.no>
2025-11-14 00:44:57 +0100

    Opprettet en klasse for User managment/Admin panel, som skal kunne ta ID, navn, bruker navn (evt bare navn hvis bruker navn er unødvendig) og rolle.

---
+++ Kartverket.Web/Models/UserManagmentModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kartverket.Web.Models
+{
+    public class UserAdminViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Role { get; set; }
+    }
+}
