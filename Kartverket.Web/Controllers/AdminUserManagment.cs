910ddc2ba19d6704b7eb013b4b4e4255520ac7c2
YonesF <yonesmf@uia.no>
2025-11-14 00:43:44 +0100

    Laget en Admin-controller som lager en liste med brukere, sender listen til cshtml som viser brukerene.

---
+++ Kartverket.Web/Controllers/AdminUserManagment.cs
@@ -0,0 +1,16 @@
+﻿using Kartverket.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class AdminPartController : Controller
+{
+    public IActionResult Index()
+    {
+        // Hent brukere
+        var users = new List<UserAdminViewModel>
+        {
+            new UserAdminViewModel { Id = 1, Name = "Test User", UserName = "testuser", Role = "Admin" }
+        };
+
+        return View("User-management", users);
+    }
+}
